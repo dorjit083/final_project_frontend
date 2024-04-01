72ea6328ec1e4642c70fc7e2b33eff1f516ba5c2
dorjit083 <dorjit083@gmail.com>
2024-04-01 18:26:06 +0600

    updated css of student page

--- src/components/pages/StudentPage.js
+++ src/components/pages/StudentPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-// import '../CSSFolder/AddStudentPage.css';
+import './StudentPage.css';
 
 export function StudentPage() {
     const [formData, setFormData] = useState({
@@ -43,34 +43,33 @@ export function StudentPage() {
 
     return (
         <div className="container">
-            <h2>Activity Form</h2>
-            <form id="activity-form" onSubmit={handleSubmit}>
+            <h2 className='addStudent_h2' >Activity Form</h2>
+            <form className='addStudent_form' id="activity-form" onSubmit={handleSubmit}>
                 <div className="form-group">
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
+                    <label className='addStudent_label' htmlFor="name">Name:</label>
+                    <input className='addStudent_input' type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="activity-id">Activity ID:</label>
-                    <input type="number" id="activity-id" name="activityId" value={formData.activityId} onChange={handleChange} />
+                    <label className='addStudent_label' htmlFor="activity-id">Activity ID:</label>
+                    <input className='addStudent_input' type="number" id="activity-id" name="activityId" value={formData.activityId} onChange={handleChange} />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="starting-date">Starting Date:</label>
-                    <input type="date" id="starting-date" name="startingDate" value={formData.startingDate} onChange={handleChange} />
+                    <label className='addStudent_label' htmlFor="starting-date">Starting Date:</label>
+                    <input className='addStudent_input' type="date" id="starting-date" name="startingDate" value={formData.startingDate} onChange={handleChange} />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="ending-date">Ending Date:</label>
-                    <input type="date" id="ending-date" name="endingDate" value={formData.endingDate} onChange={handleChange} />
+                    <label className='addStudent_label' htmlFor="ending-date">Ending Date:</label>
+                    <input className='addStudent_input' type="date" id="ending-date" name="endingDate" value={formData.endingDate} onChange={handleChange} />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="status">Status:</label>
-                    <input type="text" id="status" name="status" value={formData.status} onChange={handleChange} />
+                    <label className='addStudent_label_status' htmlFor="status">Status:</label>
+                    <input className='addStudent_input_status' type="text" id="status" name="status" value={formData.status} onChange={handleChange} />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="comments">Comments:</label>
-                    <textarea id="comments" name="comments" value={formData.comments} onChange={handleChange}></textarea>
+                    <label className='addStudent_label_comments' htmlFor="comments">Comments:</label>
+                    <textarea className='addStudent_comments' id="comments" name="comments" value={formData.comments} onChange={handleChange}></textarea>
                 </div>
-                <button onClick={handleSubmit} type="submit">Submit</button>
-                <button>New Page</button>
+                <button className='addStudent_button' onClick={handleSubmit} type="submit">Submit</button>
             </form>
         </div>
     );
